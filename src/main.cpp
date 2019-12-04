025038167c447055ed5a1b314ca77e1e9bf11c07
alekulyn <alekulyn@users.noreply.github.com>
2019-12-04 12:07:48 -0600

    Basic files

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "aco.hpp"
+
+//one ant at each starting vertex
+//each starting vertex has a pheromone that indicates its quality as a starting vertex
+//the amount of pheromone applied to each edge is also the same amount of pheromone applied to the start vertex
+
+int main() {
+}
